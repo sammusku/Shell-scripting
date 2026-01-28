6f1a5125074383f2d2270f8db5065c599dab6c8b
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 09:33:30 -0600

    variabbles

---
+++ 02.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+====change the names ===
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1 : Hi $PERSON2,How are you?"
+echo "$PERSON2 : Hi $PERSON1,im good what about you?"
+echo "$PERSON1 :thanks for asking $PERSON2,thank you."
+echo "$PERSON2 :meet you later,bye."
