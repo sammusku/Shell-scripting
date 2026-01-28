88054c4e281f4c7e9c622232fb106391a8368ffa
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 10:47:49 -0600

    syntax to cal total time the previous command ha executed

--- 05.variables.sh
+++ 05.variables.sh
@@ -2,6 +2,11 @@
 
 #===adding timestamp in sec==
 
-TIMESTAMP=$(date +%s)
+START_TIME=$(date +%s)
 
-echo "script executed at :$TIMESTAMP"
+echo "script executed at :$START_TIME"
+
+sleep 10
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "script total runtime :$TOTAL_TIME"
