9d7b9708d6dd248aa917adc4c7538a448cd7bcff
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 10:51:58 -0600

    end-time also updating

--- 05.variables.sh
+++ 05.variables.sh
@@ -8,5 +8,6 @@ echo "script executed at :$START_TIME"
 
 sleep 10
 END_TIME=$(date +%s)
+echo "scipt ended executing at :$END_TIME"
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script total runtime :$TOTAL_TIME"
