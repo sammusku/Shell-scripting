a96df90dbd604d5b0b0f6eba9ecfd475116b5bc3
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 10:08:36 -0600

    password info added

--- 04.variables.sh
+++ 04.variables.sh
@@ -5,4 +5,11 @@
 echo "please write username::"
 read USER_NAME
 
-echo "print username you have entered:$USER_NAME"
+echo "username you have entered:$USER_NAME"
+
+#===password not set to visible while entering.==
+
+echo "please enter the password to login::"
+read -s PASSWORD
+
+echo "password you have entered :$PASSWORD"
