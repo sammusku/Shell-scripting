207d0b16db6bc559af75b071a6189565926b6ac2
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 11:46:35 -0600

    arithematic

--- 07.datatypes.sh
+++ 07.datatypes.sh
@@ -8,3 +8,14 @@ NUM2=20
 SUM=$(($NUM1+$NUM2))
 
 echo "sum of the numbers:$SUM"
+
+SUB=$((NUM2-NUM1))
+
+echo "Substraction of numbers:$SUB"
+
+DIV=$((NUM2/NUM1))
+
+echo "division of number:$DIV"
+
+MUL=$((NUM1*NUM2))
+echo "multiplication of 2 numbers:$MUL"
