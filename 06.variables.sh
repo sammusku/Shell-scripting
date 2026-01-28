e1758ca7ff0f0277bf9f204c16fb7eccd8d4a931
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 11:25:49 -0600

    special variables updated

--- 06.variables.sh
+++ 06.variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #===special variables===
-echo "View ll variables passed to script:$@"
+echo "View all variables passed to script:$@"
 echo "no.of variables passed to script:$#"
 echo "script name:$0"
 echo "present working directory:$pwd"
-echo "who is working currently:$user"
+echo "who is working currently:$USER"
 sleep 10 &
-echo "PID of the executed script:$$"
-echo "background process ID:$!"
-echo "previous existing code status :$?"
+echo "PID of the executed recent script:$$"
+echo "last command executed in background process ID:$!"
+echo "executed code status :$?"
 echo "view all variables passed to script:$*"
