fb97f3e4688d68d4fa377a4a0324708d8642ae21
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 09:42:33 -0600

    args

---
+++ 03.variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#====arguments/parameters===
+
+PERSON1=1$
+PERSON2=2$
+PERSON3=3$
+
+
+echo "$PERSON1 : Hi $PERSON2,How are you?"
+echo "$PERSON2 : Hi $PERSON1,im good what about you?"
+echo "$PERSON1 :thanks for asking $PERSON2,thank you."
+echo "$PERSON2 :meet you later,bye."
