8927402f806292310ceb85960a8ac94bc52ca8b3
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 12:08:39 -0600

    conditions

---
+++ 09.conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#===syntax with conditions===
+
+NUMBER=25
+
+if [$NUMBER -gt 25];then
+echo "given number is greater than 25"
+
+elif[$NUMBER -lt 25];then
+echo "given number is less than 25"
+
+else[$NUMBER -eq 25]
+echo "given number is equal to 25
+fi
