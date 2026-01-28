04f9a58080a0dfca01dff20b97dfa0f6454acbe8
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 09:11:17 -0600

    convo

---
+++ 01.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###Just for conversation###
+
+echo "ramesh : Hi suresh,How are you?"
+echo "suresh : Hi ramesh,im good what about you?"
+echo "ramesh :thanks for asking suresh,thank you."
+echo "suresh :meet you later,bye."
