e11cfef88c4d7cdd578ba0b8e20349def8126016
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 12:31:42 -0600

    conditions newly added using math

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -13,3 +13,16 @@ echo "$NUMBER is less than 25"
 else
 echo "$NUMBER is equal to 25"
 fi
+
+USERID=$1
+
+if [ $USERID =! 1 ]; then
+echo "$USERID is ec2-user"
+
+elif [ $USERID > 1 ]; then 
+echo "$USERIND is other user"
+
+else [ $USERID == 0 ]; then
+echo "$USERUID is root user"
+
+fi
