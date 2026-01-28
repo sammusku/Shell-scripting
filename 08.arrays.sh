23fabd5d71b76404f9815635ac66bfb8d14dc194
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 11:58:31 -0600

    fruits

---
+++ 08.arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#===list of strings===
+
+FRUITS=("Apple" "Banana" "Orange")
+
+echo "write the ALL FRUITS:${FRUITS[@]}
+echo "write the first index:${FRUITS[0]}
+echo "write the 3rd index:${FRUITS[2]}
