2709d9cf6fb6f4c8f835a04f9a9542a363685c12
sammusku <Samyuktha.musku@gmail.com>
2026-01-28 11:30:43 -0600

    pid list updated

--- 06.variables.sh
+++ 06.variables.sh
@@ -6,8 +6,9 @@ echo "no.of variables passed to script:$#"
 echo "script name:$0"
 echo "present working directory:$pwd"
 echo "who is working currently:$USER"
-sleep 10 &
+sleep 10 
 echo "PID of the executed recent script:$$"
+sleep 10 &
 echo "last command executed in background process ID:$!"
 echo "executed code status :$?"
 echo "view all variables passed to script:$*"
