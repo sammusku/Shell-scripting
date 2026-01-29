ab53426cc718b9126740bf64dab926a067b070f3
sammusku <Samyuktha.musku@gmail.com>
2026-01-29 09:06:07 -0600

    logs

--- 12.logs.sh
+++ 12.logs.sh
@@ -3,12 +3,12 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
   echo "please use root user for access"
 exit 1
-  fi
+fi
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then
@@ -17,7 +17,7 @@ else
   echo "$2 ......success"
 fi 
 
-}
+ }
 
 dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "installing nginx"
@@ -27,3 +27,4 @@ VALIDATE $? "installing mysql"
 
 dnf install nodejs -y &>> $LOG_FILE
 VALIDATE $? "installing nodejs"
+
