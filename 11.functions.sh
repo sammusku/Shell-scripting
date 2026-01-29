2c1c2299baa7adaa1951d135bab4cb6f020b8410
sammusku <Samyuktha.musku@gmail.com>
2026-01-29 08:29:12 -0600

    functions

---
+++ 11.functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#===Functions are intended to complete specific task,it takes input and does the job.
+#repeated code we can keep in function() and call it whenever we need.
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "please use root user for access"
+exit 1
+  fi
+
+VALIDATE(){
+if [ $? -ne 0 ]; then
+  echo "nginx installation.....failure"
+else
+  echo "nginx installation......success"
+fi 
+
+}
+
+dnf install nginx -y
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "installing mysql"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
