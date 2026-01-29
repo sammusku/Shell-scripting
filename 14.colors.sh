7cb252f742e627c0b1808796ae7ebbc743e3562d
sammusku <Samyuktha.musku@gmail.com>
2026-01-29 11:38:00 -0600

    loops

--- 14.colors.sh
+++ 14.colors.sh
@@ -12,15 +12,15 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
- echo "$R please use root user to access $N" |tee -a $LOGS_FILE
+ echo -e "$R please use root user to access $N" |tee -a $LOGS_FILE
  exit 1
 fi
 VALIDATE(){
  if [ $1 -ne 0 ]; then
-  echo "$R $2-----failure $N" |tee -a $LOGS_FILE
+  echo -e "$R $2-----failure $N" |tee -a $LOGS_FILE
   exit 1
  else
-  echo "$G $2------success $N"
+  echo -e "$G $2------success $N"
  fi
 }
 
@@ -30,11 +30,11 @@ do
 
   dnf list installed $package -y &>> $LOGS_FILE
 if [ $? -ne 0 ]; then
-  echo "$package not installed,$Y installing now $N"
+  echo -e "$package not installed,$Y installing now $N"
   dnf install $package -y &>> $LOGS_FILE
-  VALIDATE $? "$B $package installation $N"
+  VALIDATE $? "$package installation"
 else
-  echo "$package already installed,....$B skipping $N"
+  echo -e "$package already installed,....$B skipping $N"
 fi  
 
 done 
