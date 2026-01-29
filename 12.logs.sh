abba289a28343019f5588053e74d6b8b50debdd8
sammusku <Samyuktha.musku@gmail.com>
2026-01-29 09:17:37 -0600

    logs with tee

--- 12.logs.sh
+++ 12.logs.sh
@@ -6,15 +6,15 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-  echo "please use root user for access"
+  echo "please use root user for access" |tee -a $LOGS_FILE
 exit 1
 fi
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then
-  echo "$2 .....failure"
+  echo "$2 .....failure" |tee -a $LOGS_FILE
 else
-  echo "$2 ......success"
+  echo "$2 ......success" |tee -a $LOGS_FILE
 fi 
 
  }
